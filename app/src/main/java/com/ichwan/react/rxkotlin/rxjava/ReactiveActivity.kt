e01581bab54a100831e4135cb60ee0d1d51f083b
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-16 06:25:07 +0700

    chore: rx views and register view model

--- app/src/main/java/com/ichwan/react/rxkotlin/rxjava/ReactiveActivity.kt
+++ app/src/main/java/com/ichwan/react/rxkotlin/rxjava/ReactiveActivity.kt
@@ -2,11 +2,76 @@ package com.ichwan.react.rxkotlin.rxjava
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ichwan.react.rxkotlin.R
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.ichwan.react.rxkotlin.databinding.ActivityReactiveBinding
+import com.ichwan.react.rxkotlin.rxjava.data.UserViewModel
+import com.ichwan.react.rxkotlin.rxjava.di.ServiceLocator
+import io.reactivex.rxjava3.core.Observable
 
-class ReactiveActivity : AppCompatActivity() {
+class ReactiveActivity : AppCompatActivity(), Observer<UserViewModel.UserUiState> {
+
+    private lateinit var binding: ActivityReactiveBinding
+    private val viewModel: UserViewModel by lazy {
+        ViewModelProvider(this, ServiceLocator.getUserViewModel())[UserViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reactive)
+        binding = ActivityReactiveBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.state.observe(this, this)
+
+        val refreshStream = Observable.create { emitter ->
+            binding.buttonRefresh.setOnClickListener {
+                emitter.onNext(it)
+            }
+        }
+
+        val closeStream = Observable.create { emitter ->
+            binding.suggest1.buttonClose.setOnClickListener {
+                emitter.onNext(it)
+            }
+        }
+
+        val closeStream2 = Observable.create { emitter ->
+            binding.suggest2.buttonClose.setOnClickListener {
+                emitter.onNext(it)
+            }
+        }
+
+        val closeStream3 = Observable.create { emitter ->
+            binding.suggest3.buttonClose.setOnClickListener {
+                emitter.onNext(it)
+            }
+        }
+
+        viewModel.getUsers(refreshStream, listOf(closeStream, closeStream2, closeStream3))
+    }
+
+    override fun onChanged(value: UserViewModel.UserUiState) {
+        when(value) {
+            is UserViewModel.UserUiState.User1 -> {
+                with(binding.suggest1){
+                    nameUser.text = value.data.username
+                    Glide.with(this@ReactiveActivity).load(value.data.avatar).into(imgUser)
+                }
+            }
+            is UserViewModel.UserUiState.User2 -> {
+                with(binding.suggest3){
+                    nameUser.text = value.data.username
+                    Glide.with(this@ReactiveActivity).load(value.data.avatar).into(imgUser)
+                }
+            }
+            is UserViewModel.UserUiState.User3 -> {
+                with(binding.suggest3){
+                    nameUser.text = value.data.username
+                    Glide.with(this@ReactiveActivity).load(value.data.avatar).into(imgUser)
+                }
+            }
+            is UserViewModel.UserUiState.Failed -> TODO()
+        }
     }
 }
